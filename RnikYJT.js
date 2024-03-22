1e1306c046e5673789b013042b7f0d3dea86f70d
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-03-22 17:07:19 +0800

    更新 RnikYJT.js

--- RnikYJT.js
+++ RnikYJT.js
@@ -1,17 +1,2 @@
-/*
+1/*
 
-易截图2：Rnik支持
-
-[rewrite_local]
-^https?:\/\/(jietu6|1jietu)\.com\/apiv2\/user url script-response-body https://raw.githubusercontent.com/Rnik666/Rnik-K/main/YjT.js
-
-[MITM]
-hostname = jietu6.com, 1jietu.com
-
-*/
-var guding = JSON.parse($response.body);
-guding.info.group_id = "3";
-guding.info.vip = "1";
-guding.info.group = "超级VIP一万年";
-guding.info.nickname = "Rnik";
-$done({ body: JSON.stringify(guding) });
