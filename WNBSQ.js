7677a467b054f3b22a30eb986d7fa7302a35ed8d
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-03-24 16:37:25 +0800

    更新 WNBSQ.js

--- WNBSQ.js
+++ WNBSQ.js
@@ -1,6 +1,8 @@
+/*
+
 [rewrite_local]
 # > 万能变声器 vip 
-^https?:\/\/www.40sishi.com\/voice\/user\/profile url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/wnbsq.js
+^https?:\/\/www.40sishi.com\/voice\/user\/profile url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/WNBSQ.js
 
 [mitm] 
 hostname = www.40sishi.com
@@ -13,7 +15,7 @@ hostname = www.40sishi.com
 
 var body = $response.body; 
 let obj = JSON.parse($response.body);
-obj.data.vipState.state = 1;
-obj.data.vipState.forever = true;
-obj.data.name = "https://t.me/GieGie777";
-$done({body: JSON.stringify(obj)});
+Rnik.data.vipState.state = 1;
+Rnik.data.vipState.forever = true;
+Rnik.data.name = "Rnik666 ";
+$done({body: JSON.stringify(Rnik)});
