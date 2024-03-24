b302f305527b47c187c85e8e132ed6865310760a
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-03-24 23:05:30 +0800

    更新 JJSZ.js

--- JJSZ.js
+++ JJSZ.js
@@ -2,7 +2,7 @@
 极简时钟@Rnik支持
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Guding88/Script/main/jijianshizhong.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/JJSZ.js
 
 [MITM]
 hostname = api.revenuecat.com
