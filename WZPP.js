35109892d7759dc0d3ba0adf39f4be1bb1e7b601
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-03-26 23:40:44 +0800

    更新 WZPP.js

--- WZPP.js
+++ WZPP.js
@@ -1,6 +1,7 @@
-@Rnik支持
+
 
 /*
+
 [rewrite_local]
 ^https?:\/\/bzpp2\.iwzbz\.com\/api\/.+\/(user\/getvipinfo|User\/getWXPW) url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/WZPP.js
 
@@ -11,7 +12,7 @@ hostname = bzpp2.iwzbz.com
 var Rnik = JSON.parse($response.body);
 
 chxm1023.isSuccess = true;
-chxm1023.validateKey = "chxm1023";
+chxm1023.validateKey = "Rnik666 ";
 chxm1023.data = {
     "expires" : "9999-09-09 09:09:09",
     "vipLevel" : 3
