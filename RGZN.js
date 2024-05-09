ba44203306318f95ac23726663396f3fd83f8f78
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-05-09 23:12:39 +0800

    更新 RGZN.js

---
+++ RGZN.js
@@ -0,0 +1,9 @@
+/*
+[rewrite_local] 
+^https?:\/\/jiaodelong\.top\/api\/app\/user\/getCurrentInfo url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/RGZN.js
+
+[MITM]
+hostname = jiaodelong.top
+*/
+
+{"code":200,"msg":"ok","data":{"id":"1788583692746792962","nickname":"尾号Rnik666","avatar":"https://oss.yhxykj.com/im-prod/2024/03/04/a5320a934dd44f50a422a5411eb7ed88.png","vipStatus":"1","vipLabel":"6","imgNum":999999999}}
