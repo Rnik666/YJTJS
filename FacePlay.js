548937ee6abea02b46ad61f6b78946a5cd96e002
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-05-09 10:33:41 +0800

    更新 FacePlay.js

--- FacePlay.js
+++ FacePlay.js
@@ -6,13 +6,12 @@
 hostname = api.faceplayplus.com
 
 */
-var  Rnik= JSON.parse($response.body);
+var Rnik = JSON.parse($response.body);
 
-    Rnik.data.vipExpired. = 2099-09-09,
-    Rnik. data.isAllLifeVip= 1,
-    Rnik.data.vipPassDate = 4092577010",
-    Rnik.data.isVip =  : 1,
-    Rnik.data.hasBuyGoldCoin = true,
-    Rnik.data.balance = 99999999,
+    Rnik.data.vipExpired = 4092610661000;
+    Rnik.data.isAllLifeVip= 1;
+    Rnik.data.vipPassDate = 4092610661000;
+    Rnik.data.isVip = 1;
+    Rnik.data.hasBuyGoldCoin = true;
+    Rnik.data.balance = 99999999;
   $done({body : JSON.stringify(Rnik)});
-}
