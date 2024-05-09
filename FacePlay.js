dd8487fe8c37ad0d93269dcf8087884cb8a36f02
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-05-09 10:30:35 +0800

    更新 FacePlay.js

--- FacePlay.js
+++ FacePlay.js
@@ -1,10 +1,11 @@
-
+/*
 [rewrite_local] 
 ^https?:\/\/api\.faceplayplus\.com\/users\/account url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/FacePlay.js
 
 [MITM]
 hostname = api.faceplayplus.com
 
+*/
 var  Rnik= JSON.parse($response.body);
 
     Rnik.data.vipExpired. = 2099-09-09,
