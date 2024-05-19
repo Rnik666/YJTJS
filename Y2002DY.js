5d6bf2b1ce5ddb6cf209e67ffce795bd5a40113e
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-05-19 19:53:26 +0800

    更新 Y2002DY.js

--- Y2002DY.js
+++ Y2002DY.js
@@ -1,6 +1,6 @@
 [rewrite_local]
 
-^https?:\/\/app-ios\.y2002\.com\/api\/v2\/User\/Info? url script-request-header https://raw.githubusercontent.com/Rnik666/666/main/Y1.js
+^https?:\/\/app-ios\.y2002\.com\/api\/v2\/User\/Info? url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/Y1.js
 ^https?:\/\/app-ios\.y2002\.com\/API\/V2\/Processer\.ashx? url url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/Y2.js
 
 [mitm] 
