6126f93f5c5891b61e6f6842e1b6ae3933451864
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-05-27 17:41:21 +0800

    更新 MYGF.js

---
+++ MYGF.js
@@ -0,0 +1,12 @@
+/*    魔音工坊
+      解锁永久会员
+     @Rnik666
+
+
+[rewrite_local]
+
+^https?:\/\/moyin-gateway\.moyin\.com\/moyin-account\/v1\/vip\/status? url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/M1.js
+^https?:\/\/moyin-gateway\.moyin\.com\/moyin-account\/v1\/user? url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/M2.js
+
+[mitm] 
+hostname = moyin-gateway.moyin.com
