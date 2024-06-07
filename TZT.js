a2dd9107b88bf949223bbaafd2675cb9f9da1750
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-06-07 16:21:57 +0800

    更新 TZT.js

---
+++ TZT.js
@@ -0,0 +1,16 @@
+/*        
+        ➪：图纸通
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local]
+
+^https?:\/\/api\.tuzhitong\.com\/VipManage\/User\/GetUserVipManage url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/TZT1.js
+^https?:\/\/api\.tuzhitong\.com\/Login\/UsersReferer\/GetUserDetail url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/TZT2.js
+
+[mitm] 
+hostname = api.tuzhitong.com
