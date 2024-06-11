57981146f0a6575f51dd1e06f8d69eb7ea28a8d0
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-06-11 14:32:16 +0800

    更新 LP.js

--- LP.js
+++ LP.js
@@ -10,8 +10,8 @@
 
 [rewrite_local] 
 
-^https?:\/\/lepai-api\.faceqianyan\.com\/account\/profile url script-response-body https://raw.githubusercontent.com/Rnik666/666/dd34dd3e60f9a6d4d4723915e66af9f2f33f1970/LP1.js
- ^https?:\/\/lepai-api\.faceqianyan\.com\/apple\/product url script-response-body https://raw.githubusercontent.com/Rnik666/666/dd34dd3e60f9a6d4d4723915e66af9f2f33f1970/LP2.js
+^https?:\/\/lepai-api\.faceqianyan\.com\/account\/profile url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/lp1.js
+ ^https?:\/\/lepai-api\.faceqianyan\.com\/apple\/product url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/lp2.js
 
 [MITM]
 hostname = lepai-api.faceqianyan.com
