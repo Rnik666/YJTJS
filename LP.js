cdd161424ea87de756822d9fd77425e1f26829b9
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-06-11 14:28:10 +0800

    更新 LP.js

--- LP.js
+++ LP.js
@@ -1,5 +1,6 @@
+
 /*        
-        ➪：乐拍
+        ➪：
 
         ★：解锁永久🆅🅸🅿
 
@@ -7,10 +8,11 @@
 
 𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
 
-[rewrite_local]
+[rewrite_local] 
 
 ^https?:\/\/lepai-api\.faceqianyan\.com\/account\/profile url script-response-body https://raw.githubusercontent.com/Rnik666/666/dd34dd3e60f9a6d4d4723915e66af9f2f33f1970/LP1.js
-^https?:\/\/lepai-api\.faceqianyan\.com\/apple\/product url script-response-body https://raw.githubusercontent.com/Rnik666/666/dd34dd3e60f9a6d4d4723915e66af9f2f33f1970/LP2.js
+ ^https?:\/\/lepai-api\.faceqianyan\.com\/apple\/product url script-response-body https://raw.githubusercontent.com/Rnik666/666/dd34dd3e60f9a6d4d4723915e66af9f2f33f1970/LP2.js
 
-[mitm]
+[MITM]
 hostname = lepai-api.faceqianyan.com
+
