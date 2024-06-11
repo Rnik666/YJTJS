8bd6254488101c36ebff6f1c62bfb8636b1533a1
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-06-11 14:08:58 +0800

    更新 LP.js

---
+++ LP.js
@@ -0,0 +1,16 @@
+/*        
+        ➪：乐拍
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local]
+
+^https?:\/\/lepai-api\.faceqianyan\.com\/account\/profile  url script-response-body https://raw.githubusercontent.com/Rnik666/666/dd34dd3e60f9a6d4d4723915e66af9f2f33f1970/LP1.js
+^https?:\/\/lepai-api\.faceqianyan\.com\/account\/profile  url script-response-body https://raw.githubusercontent.com/Rnik666/666/dd34dd3e60f9a6d4d4723915e66af9f2f33f1970/LP2.js
+
+[mitm]
+hostname = lepai-api.faceqianyan.com
