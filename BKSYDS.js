dc00587c42a933e641568d4ac464a955d146bc39
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-06-19 12:44:12 +0800

    更新 BKSYDS.js

---
+++ BKSYDS.js
@@ -0,0 +1,20 @@
+/*        
+        ➪：边框水印大师
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+^https?:\/\/photoby\.hasmash\.com\/app\/browseEvent url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/DS1.js
+
+^https?:\/\/photoby\.hasmash\.com\/auth\/member url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/DS2.js
+
+
+[MITM]
+hostname = photoby.hasmash.com
+
+
