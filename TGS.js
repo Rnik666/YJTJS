6f14fdce149f22916a52accec7fc767ba56b7fe9
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-06-23 01:51:15 +0800

    更新 TGS.js

---
+++ TGS.js
@@ -0,0 +1,16 @@
+/*        
+        ➪：图怪兽
+
+        ★：⚠️仅支持编辑后截图保存⚠️
+           ⚠️未支持无水印下载⚠️
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[MITM]
+hostname = api.818ps.com
+[rewrite_local] 
+^https?:\/\/api\.818ps\.com\/v4\/user\/userInfoAlls? url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/TGS1.js
+^https?:\/\/api\.818ps\.com\/v4\/pay\/vip\/list? url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/TGS2.js
+
