35ea6e2fe00efba156232bdc6b0dc362f4f10fea
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-06-24 15:41:35 +0800

    更新 GGJZBQJT.js

---
+++ GGJZBQJT.js
@@ -0,0 +1,24 @@
+/*        
+        ➪：胃之碗
+           ➪：上岸自习室
+              ➪：关不掉的闹钟
+             ➪：好好养宠物
+         ➪：呱呱记账本
+
+        𑁍:  2024-06-24
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+^https?:\/\/guagua\.beijingmorning\.cn\/index\/userinfo url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/GG.js
+^https?:\/\/room\.beijingmorning\.cn\/user\/userinfo userinfo url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/GG.js
+^https?:\/\/shoushen\.beijingmorning\.cn\/index\/userinfo userinfo url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/GG.js
+^https?:\/\/nao\.beijingmorning\.cn\/user\/userinfo userinfo url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/GG.js
+^https?:\/\/chong\.beijingmorning\.cn\/user\/userinfo userinfo url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/GG1.js
+[MITM]
+hostname = guagua.beijingmorning.cn,chong.beijingmorning.cn,room.beijingmorning.cn,shoushen.beijingmorning.cn,nao.beijingmorning.cn
+
