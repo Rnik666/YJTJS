4fc37031bf1c3d53563085bb303fcb809c628b9b
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-06-26 17:01:45 +0800

    更新 WSHB.js

---
+++ WSHB.js
@@ -0,0 +1,17 @@
+/*        
+        ➪：微商海报
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+^https?:\/\/api-poster\.ycase\.cn\/api\/user\/info url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/
+WS1.js
+^https?:\/\/api-poster\.ycase\.cn\/api\/user\/info url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/
+WS1.js 
+^https?:\/\/api-poster\.ycase\.cn\/web\/display url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/WS2.js
+[MITM]
+hostname = api-poster.ycase.cn
