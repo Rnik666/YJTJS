4ef6600694ccde04fc4fdc57410f82abcf4f127f
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-07-02 22:42:04 +0800

    更新 JJ.js

---
+++ JJ.js
@@ -0,0 +1,16 @@
+/*        
+        ➪：简单日语➕简计
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+^https?:\/\/dapi\.easyjapanese\.club\/app\/v1\/user\/profile script-response-body https://raw.githubusercontent.com/Rnik666/666/main/RY1.js
+^https?:\/\/api\.quicker\.fun\/api\/user\/profile script-response-body https://raw.githubusercontent.com/Rnik666/666/main/RY1.js
+^https?:\/\/pluto\.kaboocha\.com\/v1\/user\/info url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/RY2.js
+[MITM]
+hostname = api.quicker.fun, pluto.kaboocha.com, dapi.easyjapanese.club
+
