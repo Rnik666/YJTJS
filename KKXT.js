aad363f3ccb10bac5409649162f96362d21f0e6a
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-07-07 17:02:49 +0800

    更新 KKXT.js

---
+++ KKXT.js
@@ -0,0 +1,18 @@
+/*        
+        ➪：可可修图
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+^https?:\/\/cocamapi\.imendon\.com\/v2\/purchase\/vip\/verification url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/KK1.js
+^https?:\/\/cocamapi\.imendon\.com\/v2\/user\/profile url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/KK2.js
+
+[MITM]
+hostname = cocamapi.imendon.com
+
+
+
