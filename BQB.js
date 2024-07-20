04fa04dd686a314df4e3dfb7b2d7b1a43570f40a
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-07-20 23:57:41 +0800

    更新 BQB.js

---
+++ BQB.js
@@ -0,0 +1,17 @@
+/*        
+        ➪：表情包
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+^https?:\/\/api\.chaog\.com:9020\/Mountain_memberState url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/BQB.js
+^https?:\/\/api\.chaog\.com:9020\/Mountain_user_info_get url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/BQB1.js
+[MITM]
+hostname = api.chaog.com
+
+
