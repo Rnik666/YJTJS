d26137d714bb02ee890974cde7ab7b723026aa0c
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-10-26 09:01:09 +0800

    更新 MYGF.js

--- MYGF.js
+++ MYGF.js
@@ -7,6 +7,6 @@
 
 ^https?:\/\/moyin-gateway\.moyin\.com\/moyin-account\/v1\/vip\/status? url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/M1.js
 ^https?:\/\/moyin-gateway\.moyin\.com\/moyin-account\/v1\/user? url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/M2.js
-
+^https?:\/\/moyin-gateway\.moyin\.com\/tts-web-api url script-response-body https://raw.githubusercontent.com/Rnik666/666/main/M3.js
 [mitm] 
 hostname = moyin-gateway.moyin.com
