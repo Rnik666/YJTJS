f8fbbe4af9336d095b90c1e2021bcd52737317e7
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-11-30 18:40:35 +0800

    更新 API.js

--- API.js
+++ API.js
@@ -1,3 +1,36 @@
+/***********************************
+
+自用API类型
+
+合集
+
+***********************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 [rewrite_local]
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/Rnik666/666/refs/heads/main/api1.js
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/Rnik666/666/refs/heads/main/api2.j
