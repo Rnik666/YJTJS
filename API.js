c27c9d2eddbd4bb16dc9070ec5ee4cb87179ec5f
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-11-30 20:53:15 +0800

    更新 API.js

--- API.js
+++ API.js
@@ -61,6 +61,8 @@
 [rewrite_local]
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/Rnik666/666/refs/heads/main/api1.js
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/Rnik666/666/refs/heads/main/api2.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Rnik666/666/refs/heads/main/buy.js
 [mitm]
-hostname=api.revenuecat.com
+hostname=api.revenuecat.com,buy.itunes.apple.com
+
 
