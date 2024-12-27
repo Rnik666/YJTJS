939742b5d3238f56fa8d527f6fa74f9abd567508
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2024-12-27 17:16:33 +0800

    更新 JPFYG.js

---
+++ JPFYG.js
@@ -0,0 +1,18 @@
+        
+        ➪：金牌翻译官
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+^https?:\/\/mv-ps\.xdplt\.com\/api\/v1\/payment\/apple\/pay\/verify\/info? url script-response-body https://raw.githubusercontent.com/Rnik666/666/refs/heads/main/FY1.js
+
+^https?:\/\/mv-ps\.xdplt\.com\/api\/v1\/user\/info? url script-response-body https://raw.githubusercontent.com/Rnik666/666/refs/heads/main/FY2.js
+
+[MITM]
+hostname = mv-ps.xdplt.com
+
