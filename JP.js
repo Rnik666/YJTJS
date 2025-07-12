dcedd27781a4b78ffc608233c25e3d5275ae15f2
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2025-07-12 21:09:23 +0800

    更新 JP.js

---
+++ JP.js
@@ -0,0 +1,29 @@
+/*        
+        ➪：JP APP
+
+       
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+^https?:\/\/api\.mercari\.jp url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/JP.js
+
+[MITM]
+hostname = api.mercari.jp
+
+*/
+/*        
+        ➪：JP APP
+
+       
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+^https?:\/\/api\.mercari\.jp url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/JP.js
+
+[MITM]
+hostname = api.mercari.jp
+
+*/
+;eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('2 0=$3.0;0=0.4(/("5"\\1*:\\1*")[^"]*"/6,\'$7"\');$8({0});',9,9,'body|s|let|response|replace|result|g|1Rnik6666|done'.split('|'),0,{}));
