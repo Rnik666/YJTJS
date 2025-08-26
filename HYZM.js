70b379749b5c6018a33e91223a510c1110158220
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2025-08-26 12:46:14 +0800

    更新 HYZM.js

---
+++ HYZM.js
@@ -0,0 +1,21 @@
+/*        
+        ➪：[绘影字幕]
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+^https?:\/\/api\.zeemo\.ai\/hy-caption-front\/api\/v1\/subscription\/get-pro-user-info url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/HYZM.js
+(后戳)
+
+[MITM]
+hostname = api.zeemo.ai
+
+
+*/
+
+;eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('f 7=a.g($h.b);7={"i":0,"j":{"k":1,"l":0.c,"m":6,"n":6,"o":d,"p":6,"q":r,"s":6,"t":8,"u":6,"v":0.w,"x":9,"y":6,"z":9,"A":e,"B":3,"C":D,"E":F,"G":9,"H":1,"I":4,"J":1,"K":0.c,"L":1,"M":{"N":O,"P":1,"Q":R,"S":0,"T":1,"U":V,"W":1,"X":8,"Y":1,"Z":10,"11":"12-13端免费用户权益","14":1,"15":1,"16":1,"17":2,"18":1,"19":-1},"1a":5,"1b":8,"1c":6,"1d":6,"1e":6},"1f":"1g","1h":d,"1i":1j,"1k":e}$1l({b:a.1m(7)});',62,85,'||||||9999999|DG|2147483647|false|JSON|body|00001|true|null|var|parse|response|code|data|productLevel|captionUnitMinCreditsNeedPay|aiVideoLeftFreeCnt|totalCredits|adminConfig|leftFreeCnt|invalidDate|4092610661000|leftAdsRewardCntV2|leftCnt|leftCredits|advancedAiVideoUnitMinNeedPay|000001|isLimitIndulgence|leftFreeDownloadCnt|trialPeriod|productId|userType|basicImgToVideoUnitMinNeedPay|200|canPayCaptionDuration|6000000|isInGracePeriod|subscribePlatform|basicAiVideoUnitMinNeedPay|renewalType|advancedImgToVideoUnitMinNeedPay|proType|privilege|createdTime|1708580919000|dynamicCaptions|cloudStorageTime|30|enableDownloadCaption|privilegeType|modifiedTime|1710929214000|enableBatchUpload|exportMaxCnt|removeAds|videoMaxDuration|999999|remark|v6|app|uploadFont|translateCaption|addFreeWatermark|maxExportQuality|multiLoginCnt|monthlyCaptionQuota|leftAdsRewardCnt|totalCnt|totalDuration|maxFreeUseCnt|leftDuration|message|OK|success|serverTime|1756176218441|page|done|stringify'.split('|'),0,{}));
