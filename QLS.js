8df5d111de3720e11089ffc95f3239573507dbaf
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2025-09-08 00:13:18 +0800

    更新 QLS.js

---
+++ QLS.js
@@ -0,0 +1,20 @@
+/*        
+        ➪：全历史
+
+        ★：解锁永久🆅🅸🅿
+
+        𖣘： @𝙍𝙣𝙞𝙠➏➏➏
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+^https?:\/\/playvideo\.qcloud\.com\/getplayinfo|^https?:\/\/speed\.allhistory\.com\/ap url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/QLS.js
+
+[MITM]
+hostname = playvideo.qcloud.com, speed.allhistory.com
+
+
+*/
+
+;eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('a 2=$10.2;2=2.5(/("11"\\s*:\\s*)\\d+/g,\'$12\');2=2.5(/("13"\\s*:\\s*")[^"]*"/g,\'$y://z.A.B/C/D/E/18/F.G"\');2=2.5(/("15"\\s*:\\s*")[^"]*"/g,\'$y://z.A.B/C/D/E/18/F.G"\');2=2.5(/("16"\\s*:\\s*")[^"]*"/g,\'$1𝐓𝐆@𝐢𝐎𝐒𝐀𝐖𝐉"\');2=2.5(/("17"\\s*:\\s*)\\d+/g,\'$14\');2=2.5(/("H"\\s*:\\s*)\\w+/g,\'$j\');2=2.5(/("I"\\s*:\\s*)\\w+/g,\'$j\');2=2.5(/("J"\\s*:\\s*)\\w+/g,\'$j\');2=2.5(/("K"\\s*:\\s*)\\w+/g,\'$j\');2=2.5(/("19"\\s*:\\s*)(1a|\\d+)/g,\'$1b"\');1c{b=r.1d(2)}1e(e){$l({2});c}m(b);n(b);$l({2:r.L(b)});M m(o){4(!o||9 o!==\'p\')c;4(t.u(o)){o.N(m);c}O(a 3 P o){4(!Q.R.S.T(o,3))6;a 8=o[3];4(/^(J|1f|1g|I|H|1h)$/i.7(3)){o[3]=U;6}4(/^K$/i.7(3)){o[3]=1;6}4(/^1i$/i.7(3)){o[3]=U;6}4(/^1j$/i.7(3)){o[3]=x;6}4(/^1k$/i.7(3)){o[3]=x;6}4(/^(1l?|1m|1n|1o|1p)(1q|1r|1s)?$/i.7(3)){o[3]=1t;6}4(/(1u|1v|1w|1x)(1y|1z|1A)?$/i.7(3)){o[3]=0;6}4(/^(1B|1C)?(?:1D|1E|1F|V|1G|1H|1I|1J|1K)s?$/i.7(3)){4(/^(1L|1M|V|1N)$/i.7(3)){o[3]=x}1O{o[3]=9 8===\'1P\'?0:\'\'}6}4(3===\'1Q\'&&9 8===\'W\'&&8.1R(\'-X-Y-Z\')){o[3]=8.5(/-X-Y-Z:\\s*\\d+;/g,\'\');6}4(8&&9 8===\'p\'){m(8)}}}M n(o){4(!o||9 o!==\'p\')c;4(t.u(o)){o.N(n);c}4(Q.R.S.T(o,\'f\')&&o.f===\'\'&&o.h&&t.u(o.h.q)&&o.h.q[0]&&9 o.h.q[0].f===\'W\'){o.f=o.h.q[0].f}O(a k P o){a v=o[k];4(v&&9 v===\'p\')n(v)}}$l({2:r.L(b)});$l({2});',62,116,'||body|key|if|replace|continue|test|val|typeof|let|obj|return|||url||streamInfo||1true||done|deepUnExpire|injectUrl||object|items|JSON||Array|isArray|||false|1http|oss|v2dj|com|pic|2025|07|17528094565169|jpeg|isPermanent|hasPrivilege|vip|isFree|stringify|function|forEach|for|in|Object|prototype|hasOwnProperty|call|true|locked|string|webkit|line|clamp|response|code|1200|image||userAvatar|userName|level||expireTime|null|14092610661000|try|parse|catch|isVip|enableSubscription|purchase|showFreeTime|isShowPay|isLadderVipContent|expired|expire|end|due|valid|Time|Date|At|4102415999000|free|preview|trial|remaining|Count|Seconds|Length|need|is|paid|pay|charge|cost|price|fee|gold|coin|needPay|isLocked|isPaid|else|number|content|includes'.split('|'),0,{}));
