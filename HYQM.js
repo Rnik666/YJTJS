0b7cc0c94dab93bb5b7fde356e441d5aff9870ab
Rnik666 <141318039+Rnik666@users.noreply.github.com>
2025-09-08 10:42:48 +0800

    更新 HYQM.js

--- HYQM.js
+++ HYQM.js
@@ -9,11 +9,12 @@
 
 [rewrite_local] 
 
-^https?:\/\/www\.ssqm\.cn\/api\/ap\/v1\/mine_index\ url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/HYQM.js
+^https?:\/\/www\.ssqm\.cn\/api\/ap\/v1\/mine_index url script-response-body https://raw.githubusercontent.com/Rnik666/YJTJS/main/HYQM.js
 
 [MITM]
 hostname = www.ssqm.cn
 
 
 */
+
 ;eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('c 3=8.d($e.9);3={"f":"操作成功~","g":{"h":{"i":"j","k":l,"m":"","n":"o","p":{"a":2,"q":"r-1-1 1:1:1","s":0},"t":2,"u":2,"v":"会员用户","w":"x://y.z.A/B/4/C/D/E.F","G":0,"H":"4-1-6 b:I:J","K":"会员用户","L":0,"M":"4-1-6 b:6:N","O":"𝐓𝐆@𝐢𝐎𝐒𝐀𝐖𝐉","P":5,"Q":0},"R":{"S":7,"T":7,"U":7},"a":2,"V":[]},"W":X}$Y({9:8.Z(3)});',62,62,'|09|true|DG|2025||08|false|JSON|body|is_vip|02|var|parse|response|msg|data|user_info|id|88888|version|211|num_come_web|channel|ap|vip_info|vip_end_time|2099|have_days|is_ios|is_login|role_name|avatar|http|oss|v2dj|com|pic|07|18|17528094565169|jpeg|num_see_zi|last_seen|10|34|role|award_state|create_time|38|nickname|num_come_ap|num_see_page|can|content|trade|edit|tip_list|code|200|done|stringify'.split('|'),0,{}));
